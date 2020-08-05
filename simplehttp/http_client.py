6414c341a9bfe760d4dba8c8f23ecf76951d1702
EthanChenYen-Peng <ypcethan@gmail.com>
2020-08-05 10:56:31 +0800

    adding doc string for functions in http_client

--- simplehttp/http_client.py
+++ simplehttp/http_client.py
@@ -7,6 +7,21 @@ from urllib.parse import urlencode
 
 
 def make_request(method, url, params={}, data={}):
+    """[summary]
+
+    Args:
+        method (string): either GET or POST 
+        url (string): 
+        params (dict, optional):  Addtional parameters wish to add to the query string. Defaults to {}.
+        data (dict, optional): Request body for POST request. Defaults to {}.
+
+    Raises:
+        HttpError: [description]
+
+    Returns:
+        [type]: [description]
+    """
+
     url_parts = process_url(url, params)
     conn = http.client.HTTPSConnection(url_parts['host'])
     if (method == 'GET'):
@@ -25,22 +40,42 @@ def make_request(method, url, params={}, data={}):
 
 
 def get_json(url, params={}):
+    """API for sending GET reuqest
+
+    Args:
+        url (string): 
+        params (dict, optional): Addtional parameters wish to add to the query string. Defaults to {}.
+
+    Returns:
+        [dict]: Reponse body in JSON (Python dictionary) format
+    """
     try:
-        body = make_request('GET', url, params)
+        response = make_request('GET', url, params)
+
     except Exception as e:
         sys.last_value = e
         # re-raise the exception (allow the caller to handle)
         raise
-    return body
+    return response
 
 
 def post_json(url, params={}, data={}):
+    """API for sending POST reuqest
+
+    Args:
+        url (string): 
+        params (dict, optional): Addtional parameters wish to add to the query string. Defaults to {}.
+        data (dict, optional): Request body.
+
+    Returns:
+        [dict]: Reponse body in JSON (Python dictionary) format
+    """
     try:
-        body = make_request('POST', url, params, data)
+        response = make_request('POST', url, params, data)
     except Exception as e:
         sys.last_value = e
         raise
-    return body
+    return response
 
 # def post_json(url, params={}, data={}):
 #     url_parts = process_url(url, params)
