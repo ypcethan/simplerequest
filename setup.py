af9115bfa4a9c355a2d9cde0d1ba94d857516ac3
EthanChenYen-Peng <ypcethan@gmail.com>
2020-08-05 11:16:52 +0800

    dump patch version to 1

--- setup.py
+++ setup.py
@@ -5,19 +5,18 @@ with open("README.md", 'r') as fid:
 
 setup(
     name="ethanypc-simplerequest",
-    version="1.0.0",
+    version="1.0.1",
     description="Wrapper around http.client",
     long_description=README,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/EthanChenYen-Peng/simplerequest",
     author="Ethan",
     author_email="ypcethan@gmail.com",
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
     ],
     packages=["simplehttp"],
 )
