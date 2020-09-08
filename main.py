be498f96bf4dd2a9c6279caa21232221fa99aead
ethanchen <ethanchen@kkbox.com>
2020-09-08 16:30:04 +0800

    Switch to pipenv from virtualenv. Adding pytest and pylint to dev dependencies.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def case2():
     url_target = "https://httpbin.org/get?debug=true"
     params = {"name": u"常⾒見見問題 q&a"}
     r = simplehttp.get_json(url_target, params)
+    print(simplehttp)
     assert r['args'] == {'debug': 'true', 'name': u'常⾒見見問題 q&a'}
 
 
