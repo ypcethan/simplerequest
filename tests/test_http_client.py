350c8aaa53e31dcd81ae3be0c3a1de2cf4329cdb
ethanchen <ethanchen@kkbox.com>
2020-09-10 10:17:17 +0800

    Correct variable name in test_request. Adding pytest-mock as dev dependency.

--- tests/test_http_client.py
+++ tests/test_http_client.py
@@ -1,60 +1,17 @@
-# from __future__ import unicode_literals
-# # coding=utf-8
-# import sys
-# import json
-# import pytest
-# from simplehttp import get_json, post_json
-# from simplehttp.error import HttpError
-#
-#
-# @pytest.mark.parametrize('url,params,expected_result', [
-#     ('https://httpbin.org/get', {}, {}),
-#     ('https://httpbin.org/get?debug=true', {}, {'debug': 'true'}),
-#     ('https://httpbin.org/get?debug=true', {'limit': '20'}, {'debug': 'true',
-#                                                              'limit': '20'})
-# ])
-# def test_get_json(url, params, expected_result):
-#     response = get_json(url, params)
-#     assert response['args'] == expected_result
-#
-#
-# @pytest.mark.parametrize('url,params,data,expected_args, expected_data', [
-#     ('https://httpbin.org/post', {}, {}, {}, {}),
-#     ('https://httpbin.org/post?debug=true', {}, {}, {'debug': 'true'}, {}),
-#     ('https://httpbin.org/post?debug=true', {}, {
-#         "isbn": "9789863479116", "title": "流暢的 Python"
-#     }, {'debug': 'true'}, {
-#         "isbn": "9789863479116", "title": "流暢的 Python"
-#     }),
-#     ('https://httpbin.org/post?debug=true', {}, {
-#         "isbn": "9789863479116", "title": "常⾒見見問題 Q&A"
-#     }, {'debug': 'true'}, {
-#         "isbn": "9789863479116", "title": "常⾒見見問題 Q&A"
-#     }),
-# ])
-# def test_post_json(url, params, data, expected_args, expected_data):
-#     response = post_json(url, params, data)
-#     assert response['args'] == expected_args
-#     assert json.loads(response['data']) == expected_data
-#
-#
-# @pytest.mark.parametrize('url,error_code', [
-#     ('https://httpbin.org/status/400',  400),
-#     ('https://httpbin.org/status/500',  500),
-# ])
-# def test_post_json_error_code(url,  error_code):
-#     with pytest.raises(HttpError) as e:
-#         response = post_json(url)
-#     assert e.value.message == "HTTP Status Code: %s" % str(error_code)
-#     assert sys.last_value.status_code == error_code
-#
-#
-# @pytest.mark.parametrize('url,error_code', [
-#     ('https://httpbin.org/status/400',  400),
-#     ('https://httpbin.org/status/500',  500),
-# ])
-# def test_get_json_error_code(url,  error_code):
-#     with pytest.raises(HttpError) as e:
-#         response = get_json(url)
-#     assert e.value.message == "HTTP Status Code: %s" % str(error_code)
-#     assert sys.last_value.status_code == error_code
+
+from __future__ import unicode_literals
+# coding=utf-8
+import sys
+import json
+import pytest
+from simplehttp.request import http_get, http_post
+from simplehttp.http_client import get_json, post_json
+from simplehttp.error import HttpError
+
+
+@pytest.fixture
+def test_get_json(mocker):
+    mocker.patch('http_get', return_value=5)
+    actual = http_get()
+    expected = 6
+    assert expected == actual
--- tests/test_request.py
+++ tests/test_request.py
@@ -13,7 +13,7 @@ from simplehttp.error import HttpError
     ('https://httpbin.org/get?debug=true', {'limit': '20'}, {'debug': 'true',
                                                              'limit': '20'})
 ])
-def test_get_json(url, params, expected_result):
+def test_http_get(url, params, expected_result):
     response = http_get(url, params)
     assert response['args'] == expected_result
 
@@ -32,7 +32,7 @@ def test_get_json(url, params, expected_result):
         "isbn": "9789863479116", "title": "常⾒見見問題 Q&A"
     }),
 ])
-def test_post_json(url, params, data, expected_args, expected_data):
+def test_http_post(url, params, data, expected_args, expected_data):
     response = http_post(url, params, data)
     assert response['args'] == expected_args
     assert json.loads(response['data']) == expected_data
@@ -42,7 +42,7 @@ def test_post_json(url, params, data, expected_args, expected_data):
     ('https://httpbin.org/status/400',  400),
     ('https://httpbin.org/status/500',  500),
 ])
-def test_get_json_error_code(url,  error_code):
+def test_http_get_error_code(url,  error_code):
     with pytest.raises(HttpError) as e:
         response = http_get(url)
     assert e.value.message == "HTTP Status Code: %s" % str(error_code)
@@ -53,7 +53,7 @@ def test_get_json_error_code(url,  error_code):
     ('https://httpbin.org/status/400',  400),
     ('https://httpbin.org/status/500',  500),
 ])
-def test_post_json_error_code(url,  error_code):
+def test_http_post_error_code(url,  error_code):
     with pytest.raises(HttpError) as e:
         response = http_post(url)
     assert e.value.message == "HTTP Status Code: %s" % str(error_code)
